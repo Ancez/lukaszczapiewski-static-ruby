50bd1b727b9ffc57c52a74151249f65012072248
Lukasz Czapiewski <32838548+Ancez@users.noreply.github.com>
2025-11-22 18:24:35 +0000

    improve styles and pages

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -14,5 +14,26 @@ SitemapGenerator::Sitemap.create do
         priority: metadata[:priority] || 0.5,
         changefreq: metadata[:changefreq] || 'weekly'
   end
+
+  # Add blog posts
+  PageHelpers::BLOG_POSTS.each do |post|
+    add "/blog/#{post[:slug]}",
+        lastmod: Time.now,
+        priority: 0.7,
+        changefreq: 'monthly'
+  end
+
+  # Add project pages - automatically discover from filesystem
+  projects_dir = File.join(Dir.pwd, 'app', 'views', 'pages', 'projects')
+  if Dir.exist?(projects_dir)
+    Dir.glob(File.join(projects_dir, '*.html.erb')).each do |file|
+      slug = File.basename(file, '.html.erb')
+      next if slug.start_with?('_') # Skip partials
+      add "/projects/#{slug}",
+          lastmod: Time.now,
+          priority: 0.7,
+          changefreq: 'monthly'
+    end
+  end
 end
 
